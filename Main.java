a97e0f7974892ae2367117828e3c8e5c2a4315cd
Evgeniy9595 <eostapenko589@gmail.com>
2025-12-02 15:17:47 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,33 +1,69 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Введи первое число: ");
-        double a = sc.nextDouble();
+        while (true) {
+            String s = sc.nextLine();
 
-        System.out.print("Введи оператор (+ - * /): ");
-        String op = sc.next();
+            try {
+                String result = calc(s);
+                System.out.println(result);
+            } catch (Exception e) {
+                System.out.println("throws Exception");
+                break;
+            }
+        }
+    }
 
-        System.out.print("Введи второе число: ");
-        double b = sc.nextDouble();
+    public static String calc(String input) throws Exception {
 
-        double result = 0;
+        input = input.trim();
+        String[] m = input.split(" ");
 
-        if (op.equals("+")) {
-            result = a + b;
-        } else if (op.equals("-")) {
-            result = a - b;
-        } else if (op.equals("*")) {
-            result = a * b;
-        } else if (op.equals("/")) {
-            result = a / b;
+        if (m.length != 3) {
+            throw new Exception();
         }
 
-        System.out.println("Ответ: " + result);
+        String aStr = m[0];
+        String op = m[1];
+        String bStr = m[2];
+
+        int a, b;
+
+        try {
+            a = Integer.parseInt(aStr);
+            b = Integer.parseInt(bStr);
+        } catch (Exception e) {
+            throw new Exception();
+        }
+
+        if (a < 1 || a > 10 || b < 1 || b > 10) {
+            throw new Exception();
+        }
+
+        int result;
+
+        switch (op) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default:
+                throw new Exception();
+        }
+
+        return String.valueOf(result);
     }
 }
-
-
